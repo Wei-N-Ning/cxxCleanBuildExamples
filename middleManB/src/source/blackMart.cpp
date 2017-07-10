d19400c6d02461c25c5e2ecc0ae2a16072ee457d
Wei Ning <macgnw@gmail.com>
2017-07-09 23:33:40 -0700

    STAGE-2:
     - there are two copies of treasure static library, V1 and V2; V1 has a slightly different ABI
    interface of HolyGrail than V2
    
     - each middleman use an unique version of the treasure static library (they are still
    statically linking to the treasure libraries)
    
     - if the caller only builds treasure V1, V2 and both middlemen, there seems no problem
    
     - if the caller builds client, statically linking in both middlemen, on Cygwin there is no
    compilation/linking error, but once executed, it throws a segfault; on U16 VPS, there is also
    no build error, but once executed it behaves wildly, e.g.
    
    ```
    holy grail is empty: 224 (??!!)
    holy grail is empty: 0```
    ```
    
     - (I GUESS) there are now two copies of HolyGrail ABIs - conflict!

---
+++ middleManB/src/source/blackMart.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Wei on 9/07/2017.
+//
+
+#include "blackMart.hpp"
+
+
+treasure::HolyGrail middleManB::makeHolyGrail(bool toFill) {
+    treasure::HolyGrail holyGrail(toFill);
+    return holyGrail;
+}
